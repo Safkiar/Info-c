3ec834af306618147a64829458dcf42baa929657
Safkiar <m.kikowski97@gmail.com>
2025-10-18 20:22:15 +0200

    sunday

---
+++ 1 C/1.Hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// uruchom przez F5
+// dyrektywa preprocesora dołącza nagłówek z deklaracjami funkcji wejścia/wyjścia
+int main() {
+    // punkt wejścia programu
+    printf("Hello, World");
+    // ; kończy instrukcje i deklaracje
+    return 0;
+    // kod wyjścia programu 0 = sukces, wartości różne od zera to błąd
+}
+
+
